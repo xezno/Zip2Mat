283332788a02e6fb5159e998aaed38bc6a30c444
Alex <alex@gu3.me>
2021-04-19 21:30:20 +0100

    Move material location to config file

--- Zip2Mat/MatGen.cs
+++ Zip2Mat/MatGen.cs
@@ -1,52 +1,31 @@
 ﻿using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
 
 namespace Zip2Mat
 {
     public static class MatGen
     {
-        private const string matLocation = @"C:\Program Files (x86)\Steam\steamapps\common\Half-Life Alyx\content\hlvr_addons\sandbox_maps\materials";
-
         public static void Generate(string fileName, string matName, bool convertToTga = false, bool normalizeFileNames = false)
         {
-            Trace.WriteLine($"Reading file {fileName}");
-
-            var zipFile = ZipFile.OpenRead(fileName);
-            Trace.WriteLine($"Using material name {matName}");
+            var zipArchive = ZipFile.OpenRead(fileName);
 
             // Enumerate textures / types
             Dictionary<string, TextureData> textureDictionary = new Dictionary<string, TextureData>();
-            foreach (var file in zipFile.Entries)
+            foreach (var file in zipArchive.Entries)
             {
                 var textureName = file.Name;
                 var textureData = new TextureData(textureName);
                 textureDictionary.Add(textureData.type.ToString(), textureData);
             }
 
-            // Convert texture dict to collection
-            var textures = new TextureCollection();
-            foreach (var property in typeof(TextureCollection).GetProperties())
-            {
-                if (textureDictionary.TryGetValue(property.Name, out var val))
-                {
-                    property.SetValue(textures, $"materials/{matName}/{val.name}");
+            // Convert texture dict to texture collection
+            var textures = new TextureCollection(matName, textureDictionary, zipArchive);
 
-                    // Copy material to HLA location
-                    Directory.CreateDirectory(Path.Combine(matLocation, matName));
-                    var destPath = Path.Combine(matLocation, matName, val.name);
+            // Write to vmat
+            File.WriteAllText(Path.Combine(MainSettings.Default.MatLocation, $"{matName}.vmat"), GenerateMaterial(textures));
 
-                    zipFile.Entries.Where(t => t.Name == val.name).First().ExtractToFile(destPath, overwrite: true);
-                }
-            }
-
-            Trace.WriteLine(GenerateMaterial(textures));
-
-            File.WriteAllText(Path.Combine(matLocation, $"{matName}.vmat"), GenerateMaterial(textures));
-
-            zipFile.Dispose();
+            zipArchive.Dispose();
         }
 
         private static string GenerateMaterial(TextureCollection textures)
--- Zip2Mat/TextureCollection.cs
+++ Zip2Mat/TextureCollection.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
 
 namespace Zip2Mat
 {
@@ -12,5 +13,23 @@ namespace Zip2Mat
         public string Roughness { get; set; } = null;
         public string AmbientOcclusion { get; set; } = null;
         public string Height { get; set; } = null;
+
+
+        public TextureCollection(string matName, Dictionary<string, TextureData> textureDictionary, ZipArchive zipArchive)
+        {
+            foreach (var property in typeof(TextureCollection).GetProperties())
+            {
+                if (textureDictionary.TryGetValue(property.Name, out var val))
+                {
+                    property.SetValue(this, $"materials/{matName}/{val.name}");
+
+                    // Copy material to HLA location
+                    Directory.CreateDirectory(Path.Combine(MainSettings.Default.MatLocation, matName));
+                    var destPath = Path.Combine(MainSettings.Default.MatLocation, matName, val.name);
+
+                    zipArchive.Entries.Where(t => t.Name == val.name).First().ExtractToFile(destPath, overwrite: true);
+                }
+            }
+        }
     }
 }
